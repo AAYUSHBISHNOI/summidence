24fb3545e01203c2dbfab534d784b7f5f2a84109
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-09-11 16:53:13 +0530

    matomo-added-5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+
 import Summidence from "./Components/pages/Summidence";
 import School from "./Components/pages/School";
 import Aboutus from "./Components/pages/Aboutus";
@@ -11,19 +13,35 @@ import Kids from "./Components/pages/Kids";
 import OurContactUs from "./Components/pages/CommonContactUs";
 import UpcomingPrograms from "./Components/common/UpcomingPrograms";
 
+import matomo from "./matomo";
+
+// Custom hook for Matomo page tracking
+function useMatomoPageTracking() {
+  const location = useLocation();
+
+  useEffect(() => {
+    matomo.trackPageView({
+      documentTitle: document.title,
+    });
+  }, [location]);
+}
+
+
 function App() {
+  // Call Matomo tracking hook
+  useMatomoPageTracking();
+
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Summidence />} />
-        
         <Route path="/school" element={<School />} />
         <Route path="/about-us" element={<Aboutus />} />
         <Route path="/Business" element={<Business />} />
         <Route path="/kids" element={<Kids />} />
         <Route path="/our-contact-us" element={<OurContactUs />} />
-        <Route path="/upcoming-programs" element={<UpcomingPrograms/>} />
+        <Route path="/upcoming-programs" element={<UpcomingPrograms />} />
       </Routes>
       <Footer />
     </>
--- src/matomo.js
+++ src/matomo.js
@@ -1,20 +1,19 @@
-// src/matomo.js
 import MatomoTracker from '@datapunt/matomo-tracker-js';
 
 const matomo = new MatomoTracker({
-  urlBase: 'https://analytics.summidence.com.au/matomo/',  // ⚠️ Include trailing slash, and use full URL
-  siteId: 1,
+    urlBase: 'https://analytics.summidence.com.au/matomo/',  // trailing slash ✅ correct
+    siteId: 1,
 
-  heartBeat: {
-    active: true,
-    seconds: 10
-  },
+    heartBeat: {
+        active: true,
+        seconds: 10
+    },
 
-  linkTracking: true,
+    linkTracking: true,
 
-  configurations: {
-    setDocumentTitle: document.title
-  }
+    configurations: {
+        setDocumentTitle: document.title
+    }
 });
 
 export default matomo;
