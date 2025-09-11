791de9220f9a2c8d8084680d126e32bc884c83ce
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-09-11 17:15:31 +0530

    matomo-added-6

--- src/matomo.js
+++ src/matomo.js
@@ -2,19 +2,19 @@
 import MatomoTracker from '@datapunt/matomo-tracker-js';
 
 const matomo = new MatomoTracker({
-    urlBase: 'https://analytics.summidence.com.au/matomo/', // ✅ Use full and public Matomo URL with trailing slash
-    siteId: 1, // 🔁 Replace if you’re tracking a different site in Matomo
+    urlBase: 'https://www.summidence.com.au/matomo/',
+    siteId: 1,
 
-    // Optional: Track time spent on page
+
     heartBeat: {
         active: true,
         seconds: 10
     },
 
-    // Optional: Enable automatic link tracking
+
     linkTracking: true,
 
-    // Optional: Initial configuration
+
     configurations: {
         setDocumentTitle: document.title,
     }
