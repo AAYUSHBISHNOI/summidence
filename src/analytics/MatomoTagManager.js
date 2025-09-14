354aa762eb11607176d3151f593208ada0bb1513
AAYUSHBISHNOI <aayushkaswan79@gmail.com>
2025-09-14 19:46:26 +0530

    domain-added

--- src/analytics/MatomoTagManager.js
+++ src/analytics/MatomoTagManager.js
@@ -2,17 +2,17 @@
 import { useEffect } from 'react';
 
 function MatomoTagManager() {
-  useEffect(() => {
-    window._mtm = window._mtm || [];
-    window._mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
+    useEffect(() => {
+        window._mtm = window._mtm || [];
+        window._mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
 
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = 'http://34.143.245.132:8080/js/container_F5d0X0Lf.js';
-    document.head.appendChild(script);
-  }, []);
+        const script = document.createElement('script');
+        script.async = true;
+        script.src = 'https://mtm.analysis.smdnc.salescommand.ai/js/container_F5d0X0Lf.js';
+        document.head.appendChild(script);
+    }, []);
 
-  return null;
+    return null;
 }
 
 export default MatomoTagManager;
